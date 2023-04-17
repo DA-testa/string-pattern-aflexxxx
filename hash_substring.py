60f1331106c4e364a8ff9debbd21090136a2e562
Aleksis Boters 211RDB214 <125483680+aflexxxx@users.noreply.github.com>
2023-04-17 14:10:02 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,13 +1,13 @@
 # python3
 
 def read_input():
-    input_type = input().strip().upper()
+    input_type = input("Enter the input type (I/F): ").strip().upper()
 
     if input_type == 'I':
-        values = (input().rstrip(), input().rstrip())
+        values = (input("Enter the pattern: ").rstrip(), input("Enter the text: ").rstrip())
         return values
     elif input_type == 'F':
-        file_path = input().strip()
+        file_path = input("Enter the file path: ").strip()
         with open(file_path, 'r') as file:
             pattern = file.readline().rstrip()
             text = file.readline().rstrip()
