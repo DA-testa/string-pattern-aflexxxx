645199a2be441f605972230438a313a043efdb63
Aleksis Boters 211RDB214 <125483680+aflexxxx@users.noreply.github.com>
2023-04-17 16:28:50 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -4,20 +4,24 @@ def read_input():
     input_type = input().strip().upper()
 
     if input_type == 'I':
-        values = (input().rstrip(), input().rstrip())
-        return values
+        text = input().rstrip()
+        pattern = input().rstrip()
+        return text, pattern
     elif input_type == 'F':
         file_path = input().strip()
         with open(file_path, 'r') as file:
-            pattern = file.readline().rstrip()
-            text = file.readline().rstrip()
-            return pattern, text
+            contents = file.read().rstrip()
+            text, pattern = contents.split('\n')
+            return text, pattern
     else:
         print("Invalid input type.")
         return None, None
 
 def print_occurrences(output):
-    print(' '.join(map(str, output)))
+    if not output:
+        print("No occurrences found.")
+    else:
+        print(' '.join(str(pos) for pos in output))
 
 def rabin_karp_algorithm(pattern, text):
     p_len = len(pattern)
@@ -25,13 +29,16 @@ def rabin_karp_algorithm(pattern, text):
     prime = 101
     mod = int(1e9 + 7)
 
+    if p_len > t_len:
+        return []
+
     pattern_hash = 0
     text_hash = 0
     power = 1
 
     for i in range(p_len):
-        pattern_hash = (pattern_hash + (ord(pattern[i]) * power)) % mod
-        text_hash = (text_hash + (ord(text[i]) * power)) % mod
+        pattern_hash = (pattern_hash * prime + ord(pattern[i])) % mod
+        text_hash = (text_hash * prime + ord(text[i])) % mod
         if i < p_len - 1:
             power = (power * prime) % mod
 
@@ -43,9 +50,8 @@ def rabin_karp_algorithm(pattern, text):
                 positions.append(i)
 
         if i < t_len - p_len:
-            text_hash = (text_hash - (ord(text[i]) * power) + mod) % mod
-            text_hash = (text_hash * prime) % mod
-            text_hash = (text_hash + ord(text[i + p_len])) % mod
+            text_hash = (text_hash - (ord(text[i]) * power)) % mod
+            text_hash = (text_hash * prime + ord(text[i + p_len])) % mod
 
     return positions
 
@@ -53,7 +59,11 @@ def get_occurrences(pattern, text):
     if pattern is None or text is None:
         return []
 
-    return rabin_karp_algorithm(pattern, text)
+    occurrences = rabin_karp_algorithm(pattern, text)
+    if not occurrences:
+        return []
+    else:
+        return occurrences
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
